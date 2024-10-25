3463670c6d005dec6b8d62a6da91ca3b2d1aebbf
Thando <www.thandwem045@gmail.com>
2024-10-25 23:15:03 +0200

    Updated script.js added condition for zero as divider

--- scripts.js
+++ scripts.js
@@ -9,7 +9,17 @@ form.addEventListener("submit", (event) => {
   if (dividend === "" || divider === "") {     
       result.innerText = "Division not performed. Both values are required in inputs. Try again!"
       return
-  } else {
+  } else if  (divider === "0") {
+    result.innerText = "Division not performed. Invalid number provided. Try again!"
+    console.error("Error: 0 as divider")
+    console.trace()
+  }
+  
+  
+  
+  
+  
+  else {
     result.innerText = Math.floor(dividend / divider);
   }
   
