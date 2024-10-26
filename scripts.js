7c62a625e3cd2c3e73935968814c620d5d68f9ea
Thando <www.thandwem045@gmail.com>
2024-10-26 17:57:00 +0200

    Addd comments to script.js

--- scripts.js
+++ scripts.js
@@ -5,21 +5,25 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
+
+
   //Checks if inputs are empty
   if (dividend === "" || divider === "") {     
       result.innerText = "Division not performed. Both values are required in inputs. Try again!"
       return
-  } else if  (divider === "0") {
+  } 
+  // When divider is 0
+  else if  (divider === "0") {
     result.innerText = "Division not performed. Invalid number provided. Try again!"
     console.error("Error: 0 as divider")
     console.trace()
   }
-  
-  
+
   
   
   
   else {
+    // Rounds down answer
     result.innerText = Math.floor(dividend / divider);
   }
   
